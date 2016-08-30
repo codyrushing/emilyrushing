26bc93f9502060fb86f63c77b4ba042f306e098a
Cody Rushing <codyrushing@gmail.com>
2016-08-29 22:33:18 -0500

    forcing www

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const paths = require("./paths");
 const fileServer = new nodeStatic.Server(paths.distBase);
 
 require("http").createServer(function (request, response) {
+  var url = request.url;
+
+  // force www
+  if (request.headers.host === "emily-rushing.com") {
+   response.statusCode = 302;
+   response.setHeader("Location", "www."+url);
+  }
+
   request.addListener("end", function () {
     fileServer.serve(request, response);
   }).resume();
+  
 }).listen(port, (err) => {
   if (err) throw err;
   console.log(
